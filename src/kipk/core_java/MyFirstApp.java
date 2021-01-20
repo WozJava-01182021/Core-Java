221799901a17229b6f5f94a71efcccd25a8a7576
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-01-20 13:11:33 -0500

    separate my work

--- src/kipk/core_java/MyFirstApp.java
+++
@@ -1,9 +0,0 @@
-package kipk.core_java;
-
-public class MyFirstApp {
-	//below is method declaration or method signature
-	public static void main (String[] args) {
-		// "syso" + ctrl + spacebar is a shortcut for typing "System.out.ln();"
-		System.out.println("Java Rocks!");
-	}
-}
--- src/kipk/core_java/lesson01/exercises/Cat.java
+++
@@ -1,9 +0,0 @@
-package kipk.core_java.lesson01.exercises;
-
-public class Cat extends Animal {
-	
-	public Cat () {
-		type = "cat"; 
-	}
-
-}
