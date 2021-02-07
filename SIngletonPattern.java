c06ad663b373b623c37044865c1eef584ccb77f0
Kip <69485581+KipKaibel@users.noreply.github.com>
2021-02-06 18:26:49 -0600

    Update SIngletonPattern.java

--- SIngletonPattern.java
+++ SIngletonPattern.java
@@ -2,7 +2,7 @@ package robertd.core_java.lesson09;
 
 import robertd.core_java.common.Light;
 
-public class SIngletonPattern {
+public class SingletonPattern {
 	
 	public void inClassWork() {
 		System.out.println(this.getClass() + ".inClassWork():\n");
@@ -10,9 +10,22 @@ public class SIngletonPattern {
 		
 		// Can't use 'new' to instantiate the singleton
 //		Light newLight = new Light();
-		System.out.println("Light count: " + Light.count);
+		System.out.println("Before Light is loaded, Light count: " + Light.count);
+		System.out.println();
+		
+		System.out.println("Call Light.getInstance()");
 		System.out.println();
 		Light newLight = Light.getLightInstance();
+		System.out.println("Light count:" + Light.count);
+		System.out.println();	
+		System.out.println("Calling Light.getLightInstance() a second time:");
+		System.out.println();
+		Light newLight2 = Light.getLightInstance();
+		System.out.println("Light count: " + Light.count);
+		System.out.println();
+
+		
+		
 
 		System.out.println("===================================================================\n\n");
 	}
